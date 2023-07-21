41237619e409a1ad437bda63e5a17fa1ae682c3d
SerhiiBaranov <csergo228@gmail.com>
2023-07-21 21:53:00 +0300

    Update design of the login and registration pages

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -19,44 +19,115 @@ const Auth = () => {
     setPassword('');
   };
 
+  const authContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
+  const authFormStyle = {
+    backgroundColor: '#f9f9f9',
+    padding: '20px',
+    borderRadius: '5px',
+    width: '500px', // Increase the width of the form
+  };
+
+  const headingStyle = {
+    textAlign: 'center',
+    marginBottom: '20px',
+  };
+
+  const formGroupStyle = {
+    marginBottom: '15px',
+  };
+
+  const labelStyle = {
+    display: 'block',
+    marginBottom: '5px',
+  };
+
+  const inputStyle = {
+    width: '100%',
+    padding: '8px',
+    border: '1px solid #ccc',
+    borderRadius: '3px',
+  };
+
+  const buttonStyle = {
+    display: 'block',
+    width: '100%',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '3px',
+    backgroundColor: '#FAA90C',
+    color: '#000',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+    marginTop: '20px', // Add margin to the top
+  };
+
+  const authInfoStyle = {
+    textAlign: 'center',
+    marginTop: '20px',
+  };
+
+  const linkStyle = {
+    color: '#007bff',
+    textDecoration: 'none',
+  };
+
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ height: window.innerHeight - 54 }}>
-      <div style={{ backgroundColor: 'lightyellow', padding: '20px', borderRadius: '5px' }}>
-        <h2>{isLogin ? 'Login' : 'Registration'}</h2>
+    <div style={authContainerStyle}>
+      <div style={authFormStyle}>
+        <h2 style={headingStyle}>{isLogin ? 'Login' : 'Registration'}</h2>
         <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
+          <div style={formGroupStyle}>
+            <label htmlFor="username" style={labelStyle}>
+              Username:
+            </label>
             <input
               type="text"
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="form-control"
+              style={inputStyle}
+              placeholder="Enter your username"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
+          <div style={formGroupStyle}>
+            <label htmlFor="password" style={labelStyle}>
+              Password:
+            </label>
             <input
               type="password"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
+              style={inputStyle}
+              placeholder="Enter your password"
             />
           </div>
-          <button type="submit" className="btn btn-primary mt-3">
+          <button
+            type="submit"
+            style={{ ...buttonStyle }}
+            onMouseEnter={(e) => e.target.style.backgroundColor = '#FFC554'}
+            onMouseLeave={(e) => e.target.style.backgroundColor = '#FAA90C'}
+          >
             {isLogin ? 'Sign in' : 'Register'}
           </button>
         </form>
-        {isLogin ? (
-          <div>
-            Not registered yet? <NavLink to={REGISTRATION_ROUTE}>Register here!</NavLink>
-          </div>
-        ) : (
-          <div>
-            Have an account? <NavLink to={LOGIN_ROUTE}>Sign in!</NavLink>
-          </div>
-        )}
+        <div style={authInfoStyle}>
+          {isLogin ? (
+            <div>
+              Not registered yet? <NavLink to={REGISTRATION_ROUTE} style={linkStyle}>Register here!</NavLink>
+            </div>
+          ) : (
+            <div>
+              Have an account? <NavLink to={LOGIN_ROUTE} style={linkStyle}>Sign in!</NavLink>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
